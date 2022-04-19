0e016079a8db7621699d44b6fba39cb85f8d736e
AdrianG00 <giurgicatalin@gmail.com>
2022-04-19 14:49:09 +0200

    Add: jsnack1

---
+++ jsnack1/js/script.js
@@ -0,0 +1,11 @@
+// jsnack1. 
+
+let word1 = prompt("Inserisci una parola");
+let word2 = prompt("Inserisci un'altra parola");
+
+if (word1.length > word2.length) {
+    console.log(word2, word1);
+} else {
+    console.log(word1, word2);
+}
+
